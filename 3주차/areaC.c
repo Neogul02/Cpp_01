da5da916cb079732f969e8aa5fb1f47826c85371
Choi Jin Hyeung <111578210+Neogul02@users.noreply.github.com>
2023-09-18 13:04:04 +0900

    Add files via upload

---
+++ 3주차/areaC.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+double area(int r){
+    return r*r*3.14;
+}
+
+int main(void){
+    int n=3;
+    char c="#";
+    printf("#5.5-3hello1\n");
+    printf("n+5=%d\n", n+5);
+    printf("면적은 %.2f\n",area(n));
+    return 0;
+}
