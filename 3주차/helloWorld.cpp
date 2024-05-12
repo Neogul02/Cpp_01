a62288af484df2ffa496e8d33cb8f0c357b2c306
Choe JinHyeong <111578210+Neogul02@users.noreply.github.com>
2024-05-12 23:59:41 +0900

    Delete 3주차 directory

--- 3주차/helloWorld.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream> // cout과 << 연산자가 포함된 헤더파일
-using namespace std; // std:: 생략가능
-int main() // main 함수는 프로그램의 시작점
-{
-    cout << "Hello World! " << endl ; // 화면에 hello world 출력
-    cout << "첫번째 맛보기 입니다.\n"; 
-    return 0; // main 함수가 종료하면 프로그램 종료, 생략가능
-};
